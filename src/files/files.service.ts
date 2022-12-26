38f4666f10bc933d2013237dfe49b4d7c10730ae
Kostia Nahula <kostyannagula@gmail.com>
2022-12-26 20:44:02 +0200

    implement base method for the folder and file services

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { FileDocument, File } from './file.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  constructor(
+    @InjectModel(File.name)
+    private readonly fileModel: Model<FileDocument>,
+  ) {}
+
+  async createOne(data: File): Promise<File> {
+    const file = new this.fileModel(data);
+    const res = file.save();
+    return res as unknown as File;
+  }
+
+  async getOne(_id: string): Promise<File> {
+    return this.fileModel.findOne({ _id: _id }).exec() as unknown as File;
+  }
+
+  async getMany(page = 1, limit = 10): Promise<File[]> {
+    const res = this.fileModel
+      .find()
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .exec();
+    return res as unknown as File[];
+  }
+
+  async updateOne(_id: string, data: Partial<File>): Promise<File> {
+    return this.fileModel.findByIdAndUpdate(
+      { id: _id },
+      { ...data },
+    ) as unknown as File;
+  }
+
+  async deleteOne(_id: string): Promise<void> {
+    this.fileModel.findByIdAndDelete({ id: _id });
+  }
+}
--- src/folders/folders.service.ts
+++ src/folders/folders.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { FolderDocument, Folder } from './folder.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class FoldersService {}
+export class FoldersService {
+  constructor(
+    @InjectModel(Folder.name)
+    private readonly folderModel: Model<FolderDocument>,
+  ) {}
+
+  async createOne(data: Folder): Promise<Folder> {
+    const folder = new this.folderModel(data);
+    const res = folder.save();
+    return res as unknown as Folder;
+  }
+
+  async getOne(_id: string): Promise<Folder> {
+    return this.folderModel.findOne({ _id: _id }).exec() as unknown as Folder;
+  }
+
+  async getMany(page = 1, limit = 10): Promise<Folder[]> {
+    // get many
+    const res = this.folderModel
+      .find()
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .exec();
+
+    return res as unknown as Folder[];
+  }
+
+  async updateOne(_id: string, data: Partial<Folder>): Promise<Folder> {
+    return this.folderModel.findByIdAndUpdate(
+      { id: _id },
+      { ...data },
+    ) as unknown as Folder;
+  }
+
+  async deleteOne(_id: string): Promise<void> {
+    this.folderModel.findByIdAndDelete({ id: _id });
+  }
+}
